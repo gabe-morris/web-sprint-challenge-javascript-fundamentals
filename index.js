499421c449ae18d1f7e8cef715d80176424a41b3
gabe-morris <gabe.e.morris@gmail.com>
2021-06-18 14:53:17 -0500

    completed volume and surfaceArea prototypes for CuboidMaker

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ function greeting(first,last){
 */
 function CuboidMaker(dimensions){
   this.length = dimensions.length;
-  this.width = dimensions.width
+  this.width = dimensions.width;
   this.height = dimensions.height;
 }
 
@@ -176,8 +176,8 @@ function CuboidMaker(dimensions){
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
   CuboidMaker.prototype.volume = function(){
+    return this.length * this.width * this.height;
   }
-
 
 
 
@@ -185,7 +185,9 @@ function CuboidMaker(dimensions){
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
-
+  CuboidMaker.prototype.surfaceArea = function (){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
 
 
 
